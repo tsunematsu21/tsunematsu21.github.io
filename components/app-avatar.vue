08503cfe11ac950c6543b41284205ff47e69fa42
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2025-05-13 13:18:01 +0900

    modify avatar size

--- components/app-avatar.vue
+++ components/app-avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center gap-4">
     <div class="h-1 flex-1 bg-black"></div>
-    <img src="/avatar.webp" alt="Masato Tsunematsu" class="w-28 md:w-32 rounded-full border-4 border-black">
+    <img src="/avatar.webp" alt="Masato Tsunematsu" class="w-32 md:w-48 rounded-full border-4 border-black">
     <div class="h-1 flex-[0.25] bg-black"></div>
   </div>
 </template>
