85e7771e55c2053bf8ef63b4602687f1f3b07141
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2025-05-14 22:57:16 +0900

    add tooltip

--- components/social-link.vue
+++ components/social-link.vue
@@ -1,16 +1,32 @@
 <template>
    <ul class="flex flex-wrap items-start justify-center gap-8">
     <li>
-      <AppButton href="https://github.com/tsunematsu21"></AppButton>
+      <AppButton href="https://github.com/tsunematsu21">
+        <template #tooltip>
+          GitHub
+        </template>
+      </AppButton>
     </li>
     <li>
-      <AppButton href="https://instagram.com/tsunematsu21"></AppButton>
+      <AppButton href="https://instagram.com/tsunematsu21">
+        <template #tooltip>
+          Instagram
+        </template>
+      </AppButton>
     </li>
     <li>
-      <AppButton href="https://x.com/tsunematsu21"></AppButton>
+      <AppButton href="https://x.com/tsunematsu21">
+        <template #tooltip>
+          X (Twitter)
+        </template>
+      </AppButton>
     </li>
     <li>
-      <AppButton href="https://note.com/tsunematsu21"></AppButton>
+      <AppButton href="https://note.com/tsunematsu21">
+        <template #tooltip>
+          Note (Japanese)
+        </template>
+      </AppButton>
     </li>
   </ul>
 </template>
