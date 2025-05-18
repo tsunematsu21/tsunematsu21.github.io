62c8054b97121f10043b5ae373754a87f568558c
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2025-05-18 12:08:50 +0900

    add contact form base

---
+++ components/contact/form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form class="block space-y-[1rem] font-sans">
+    <ContactInput type="text" label="Name" />
+    <ContactInput type="email" label="Email" />
+    <ContactInput type="text" label="Subject" />
+    <ContactInput type="textarea" label="Message" />
+    <button class="bg-black text-white dark:bg-white dark:text-black rounded-md w-full h-8 cursor-pointer flex items-center justify-center gap-2">
+      <span class="text-xs">
+        Send Message
+      </span>
+      <svg class="w-[1em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Remix Icon by Remix Design - https://github.com/Remix-Design/RemixIcon/blob/master/License --><path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634L21.044 2.32c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8l-8 6z"/></svg>
+    </button>
+  </form>
+</template>
---
+++ components/contact/input.vue
@@ -0,0 +1,36 @@
+<template>
+  <label class="block">
+    <div class="text-xs font-sans font-bold">
+      {{ label }}:
+    </div>
+    <component :is="component.element" v-bind="{ ...component.props }" class="border border-black/50 dark:border-white/50 rounded-sm w-full px-2 py-1 text-sm" />
+  </label>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  type: "text" | "email" | "textarea",
+  label: string,
+  placeholder?: string,
+}>()
+
+const component = computed(() => {
+  if (props.type == 'textarea') {
+    return {
+      element: 'textarea',
+      props: {
+        rows: 5,
+        placeholder: props.placeholder,
+      }
+    }
+  } else {
+    return {
+      element: 'input',
+      props: {
+        type: props.type,
+        placeholder: props.placeholder,
+      }
+    }
+  }
+});
+</script>
