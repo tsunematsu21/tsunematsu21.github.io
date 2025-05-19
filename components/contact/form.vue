2cda81722a66e9164bcb39a620a103aedfef9097
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2025-05-19 12:15:41 +0900

    add note to form

--- components/contact/form.vue
+++ components/contact/form.vue
@@ -10,6 +10,9 @@
       </span>
       <svg class="w-[1em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Remix Icon by Remix Design - https://github.com/Remix-Design/RemixIcon/blob/master/License --><path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634L21.044 2.32c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8l-8 6z"/></svg>
     </button>
+    <p class="text-xs">
+      If this form doesn't work, please submit via <AppLink href="https://forms.gle/hAPgmN3eDBrWXf3f6">Google Form</AppLink>
+    </p>
   </form>
 </template>
 
